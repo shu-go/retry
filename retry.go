f99eb37e78fffeb408369058c91fce2f36c5d457
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2017-02-26 08:26:24 +0900

    add doc comments

--- retry.go
+++ retry.go
@@ -1,11 +1,15 @@
+// retry routines
 package retry
 
 import (
 	"time"
 )
 
+// Func is a function type that is passed to retry routines.
+// If it returns true, retry routines end immediately.
 type Func func() (done bool)
 
+// Count retries the function f at most count times.
 func Count(count int, f Func) bool {
 	for i := 1; i <= count; i++ {
 		done := f()
@@ -16,10 +20,13 @@ func Count(count int, f Func) bool {
 	return false
 }
 
+// Time retries the function f until the duration timeout is passed.
 func Time(timeout time.Duration, f Func) bool {
 	return Wait(timeout, 0, f)
 }
 
+// Wait retries the function f until the duration timeout is passed.
+// It also sleeps for the duration wait between each try.
 func Wait(timeout, wait time.Duration, f Func) bool {
 	start := time.Now()
 	for {
